ac1d3cda893137491d6760e40cd7abc4266440f0
Arjun-Parzival01 <einsteinarjun101@gmail.com>
2021-10-25 19:43:43 +0530

    added heroku

---
+++ build/precache-manifest.6853fad121e7d612885673aaf410545e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459f40692ccb59874661def1937ef271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90c518c7013337c7221",
+    "url": "/static/js/2.619ca51e.chunk.js"
+  },
+  {
+    "revision": "9e03c2ebe6ff0913ea1e",
+    "url": "/static/js/main.fcb88532.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
